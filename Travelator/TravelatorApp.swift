7cb330dce9251da9a3bcad16b03699c8c098ded4
quintenbuwalda <43630480+quintenbuwalda@users.noreply.github.com>
2024-11-01 14:29:01 +1100

    Initial Commit

---
+++ Travelator/TravelatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelatorApp.swift
+//  Travelator
+//
+//  Created by Quinten Buwalda on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
